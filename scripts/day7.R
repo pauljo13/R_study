246fa0406ff3c82a42368a92081ccb4c7891c912
pauljo13 <pauljbs1397@gmail.com>
2024-12-05 17:54:57 +0900

    2024-12-05

--- scripts/day7.R
+++ scripts/day7.R
@@ -61,3 +61,90 @@ rownames(A) <- paste("X", 1:5, sep = "")
 A
 apply(A, 1, min)
 apply(A, 2, max)
+
+apply(A, 2, range)
+apply(A, 1, range)
+apply(A, 1, summary)
+
+
+# arrange
+x <- 11:20; x
+x[1]
+x[6:7]
+x <- matrix(11:19, nrow = 3, ncol = 3); x
+x[1,2]
+x[2:3, 1]
+
+### array()
+x <- array(11:28, dim = c(3,3,2)); x
+
+x[2, 1, 1]
+x[2, 1, 2]
+x[1, 2:3, 2]
+x <- array(11:19, c(3,3)); x
+
+### 3 dimension
+x <- array(1, dim = c(2, 3, 2));x
+x <- array(1:6, dim = c(3, 3, 2));x
+x <- array(1:5, dim = c(3, 3, 2));x
+x <- array(1:30, dim = c(3, 3, 2));x
+
+### 4 dimension
+y <- array(1:30, dim = c(2, 3, 2, 2)); y
+
+### array is vector
+typeof(x)
+class(x)
+attributes(x)
+
+x <- 1:12
+dim(x)
+x
+dim(x) <- c(3,4)
+x
+dim(x) <- c(2, 3, 2)
+x
+dim(x) <- NULL
+x
+
+### dimnames & dimanmes()
+dim(x) <- c(2, 3, 2)
+dimnames(x) <- list(c("A", "B"),
+                    paste("group", 1:3, sep="-"),
+                    c("Male", "Female")); x
+attributes(x)
+
+## array operation and filtering
+a <- array(1:8, c(2, 2, 2)); a
+b <- array(10^(0:7), c(2,2,2)); b
+a + b
+a * b
+d <- array(11:30, c(2, 3, 2)); d
+a + d
+2 * a
+1:2 * a
+
+##### drop
+x
+x[ , , 2]
+x[1, 2:3, 2]
+x[1, 2:3, 1:2]
+x[1, 2:3, 1:2, drop=F]
+x[,-2,c(T,F)]
+x["A", "group-2", "Female"]
+
+# matrix multiplication
+A <- array(1:4, c(2,2)); A
+B <- array(10^(0:3), c(2,2)); B
+A * B
+B * A
+A %*% B
+B %*% A
+
+x <- c(1,1)
+x %*% A
+A %*% x
+x %*% A %*% x
+
+
+# diagonal
